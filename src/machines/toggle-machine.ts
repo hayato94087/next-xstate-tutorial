f873b582935993f7a8f4e6526ddb07412467cd96
hayato94087 <hayato94087@gmail.com>
2024-04-29 11:09:43 +0900

    feat:add guard

--- src/machines/toggle-machine.ts
+++ src/machines/toggle-machine.ts
@@ -1,19 +1,30 @@
-import { createMachine } from 'xstate';
+import { assign, createMachine } from "xstate";
 
 export const toggleMachine = createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoCSAdgIYDGyAlgG5gDEJm5JA1gNoAMAuoqAA6qzkKqAtxAAPRAEYAzACYcATgDsMgKwAOWWwAs03bOkAaEAE9EWtji2btGybck6Avk+NoM2HAEEyVWvUZWTlE+ASERJHEpOUUVaQ0tXX0jU0RtADYcePVpe2189XV09NkXVxACVAg4UXcsMBD+QXJhUQkEAFpinCUFWQKFdW0FNklB4zNO7UsFdNsFEdVpEqUlFzd0evxiX2pGsJaI0HaOyXUevoGhkbH1CaktRSLZnPSFSXS1dZA6zx8KPaRULNVqRdqyJSWWTqJQ5dSzaRsYopSaSR6DN7pV7vT7SNZlIA */
-  id: 'toggle',
-  initial: 'Inactive',
-  context: { count: 0 },
+  id: "toggle",
+  initial: "Inactive",
+  context: {
+    count: 0,
+    maxCount: 3,
+  },
   states: {
     Inactive: {
       // targetを省略しない記述方法
-      on: { click : { target: 'Active' } }
+      on: {
+        click: {
+          target: "Active",
+          guard: ({ context }) => context.count < context.maxCount,
+        },
+      },
     },
     Active: {
+      entry: assign({
+        count: ({ context }) => context.count + 1,
+      }),
       // targetを省略した記述方法
-      on: { click: 'Inactive' },
-      after: { 2000: 'Inactive' },
+      on: { click: "Inactive" },
+      after: { 2000: "Inactive" },
     },
   },
 });
