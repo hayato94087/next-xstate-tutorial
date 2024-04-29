2f6efdf80a6406543b61381817333dc7ff63db86
hayato94087 <hayato94087@gmail.com>
2024-04-29 10:34:05 +0900

    feat:add xstate visual editor

--- src/machines/toggle-machine.ts
+++ src/machines/toggle-machine.ts
@@ -1,14 +1,17 @@
-import { createMachine } from "xstate";
+import { createMachine } from 'xstate';
 
 export const toggleMachine = createMachine({
-  id: "toggle",
-  initial: "Inactive",
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoCSAdgIYDGyAlgG5gDEJm5JA1gNoAMAuoqAA6qzkKqAtxAAPRAEYAzACYcATgDsMgKwAOWWwAs03bOkAaEAE9EWtji2btGybck6Avk+NoM2HAEEyVWvUZWTlE+ASERJHEpOUUVaQ0tXX0jU0RtADYcePVpe2189XV09NkXVxACVAg4UXcsMBD+QXJhUQkEAFpinCUFWQKFdW0FNklB4zNO7UsFdNsFEdVpEqUlFzd0evxiX2pGsJaI0HaOyXUevoGhkbH1CaktRSLZnPSFSXS1dZA6zx8KPaRULNVqRdqyJSWWTqJQ5dSzaRsYopSaSR6DN7pV7vT7SNZlIA */
+  id: 'toggle',
+  initial: 'Inactive',
   states: {
     Inactive: {
-      on: { type: "toggle", target: "Active" },
+      // targetを省略しない記述方法
+      on: { click : { target: 'Active' } }
     },
     Active: {
-      on: { type: "toggle", target: "Inactive" },
+      // targetを省略した記述方法
+      on: { click: 'Inactive' },
     },
   },
 });
