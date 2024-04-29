c50bf80d6159cdf91814c7bf5625992d26839246
hayato94087 <hayato94087@gmail.com>
2024-04-29 10:44:32 +0900

    feat:add context

--- src/machines/toggle-machine.ts
+++ src/machines/toggle-machine.ts
@@ -4,6 +4,7 @@ export const toggleMachine = createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoCSAdgIYDGyAlgG5gDEJm5JA1gNoAMAuoqAA6qzkKqAtxAAPRAEYAzACYcATgDsMgKwAOWWwAs03bOkAaEAE9EWtji2btGybck6Avk+NoM2HAEEyVWvUZWTlE+ASERJHEpOUUVaQ0tXX0jU0RtADYcePVpe2189XV09NkXVxACVAg4UXcsMBD+QXJhUQkEAFpinCUFWQKFdW0FNklB4zNO7UsFdNsFEdVpEqUlFzd0evxiX2pGsJaI0HaOyXUevoGhkbH1CaktRSLZnPSFSXS1dZA6zx8KPaRULNVqRdqyJSWWTqJQ5dSzaRsYopSaSR6DN7pV7vT7SNZlIA */
   id: 'toggle',
   initial: 'Inactive',
+  context: { count: 0 },
   states: {
     Inactive: {
       // targetを省略しない記述方法
